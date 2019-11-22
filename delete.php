d41fd354fca9be38b2085b28982f4d0350c7abcf
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-11-21 22:16:33 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,21 +1,16 @@
 <?php
 require 'inc/functions.php';
-
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $page = 'Delete Entry';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 }
-
 if (delete_entry($id)) {
     echo 'Successfully deleted entry.';
     header('refresh: 1; url = index.php');
-    
 } else {
-    
     echo 'Unable to delete entry. Try again.';
     header('refresh: 1; url = detail.php?id="' . $id . '"');
 }
-
 include 'inc/header.php';
 include 'inc/footer.php';

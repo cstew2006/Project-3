037b603b890ff61be792700e158c7f78df4288cf
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-12-04 23:04:31 -0500

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,9 @@
- <footer>
-      <div>
-        &copy;<?php echo date("Y"); ?> MyJournal
-      </div>
-    </footer>
-  </body>
+      </div> <!-- end container -->
+        </section>
+        <footer>
+            <div>
+                &copy; <?php echo date('Y'); ?> MyJournal
+            </div>
+        </footer>
+    </body>
 </html>

af7c844746b85f62f87db51bdeaae5e7d12b08d3
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-12-04 23:03:59 -0500

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -1,7 +1,10 @@
 <?php
+
 try {
-    $db = new PDO("sqlite:".__DIR__."/journal.db");
+    $db = new PDO('sqlite:'.__DIR__.'/journal.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
+    echo 'Unable to connect 😕 <br>';
     echo $e->getMessage();
+    exit;
 }

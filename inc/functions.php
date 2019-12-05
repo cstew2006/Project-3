6ab5b98d1315735c65aa09d7db40fb622248cddd
cstew2006 <53883455+cstew2006@users.noreply.github.com>
2019-12-04 23:07:32 -0500

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,109 +1,228 @@
 <?php
-function get_entry(){
-    include ("connection.php");
+
+
+function get_list_entries() 
+{
+    include 'connection.php';
+
+    $sql = "SELECT  entries.id, entries.title, entries.date, GROUP_CONCAT(tags.name, ' ') AS name FROM entries
+        LEFT OUTER JOIN the_link ON entries.id = the_link.entry_id 
+        LEFT OUTER JOIN tags ON the_link.tags_id = tags.id
+        GROUP BY entries.id ORDER BY date DESC";
+
     try {
-        $sql = "SELECT * FROM entries ORDER BY date DESC";
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $results;
+        return $db->query($sql);
     } catch (Exception $e) {
-        echo "Unable to query DB" . $e->getMessage();
-        die();
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        return [];
     }
 }
-function create_entry($title, $date, $time_spent, $learned, $resources, $tagId = NULL) {
-    include ("connection.php");
-    
-    
-        $sql = "INSERT INTO entries (title, date, time_spent, learned, resources) VALUES (:title, :date, :time_spent, :learned, :resources)";
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam(':title',$title,PDO::PARAM_STR);
-        $stmt->bindParam(':date',$date);
-        $stmt->bindParam(':time_spent',$time_spent,PDO::PARAM_STR);
-        $stmt->bindParam(':learned',$learned,PDO::PARAM_STR);
-        $stmt->bindParam(':resources',$resources,PDO::PARAM_STR);
-        $stmt->execute();
-        $entryId = $db->lastInsertId();
-        
-            }
-            return false;
-        
-    
-    if (1 == $stmt->rowCount()) {
-        {
-            return true;
+
+function pull_tags() 
+{
+    include 'connection.php';
+
+    try {
+
+
+        $tagsArray = [];
+        $tags = $db->query('SELECT name FROM tags')->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($tags as $tag) {
+            $tagsArray[] = $tag['name'];
         }
+        return $tagsArray;
+    } catch (Exception $e) {
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        return [];
+    }
+}
+
+function add_entry($title, $date, $timeSpent, $learned, $resources = null, $tags = null)
+{
+    include 'connection.php';
+
+    try {
+
+
+        $db->beginTransaction();
+
+        $sql = 'INSERT INTO entries (title, date, time_spent, learned, resources) VALUES (?, ?, ?, ?, ?)';
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $title, PDO::PARAM_STR);
+        $results->bindValue(2, $date, PDO::PARAM_STR);
+        $results->bindValue(3, $timeSpent, PDO::PARAM_STR);
+        $results->bindValue(4, $learned, PDO::PARAM_STR);
+        $results->bindValue(5, $resources, PDO::PARAM_STR);
+        $results->execute();
+        $entry_id = $db->lastInsertId();
+
+        foreach ($tags as $tag) {
+
+            if (isset($tag) && (in_array($tag, get_tags())) == false) {
+                $sql = 'INSERT INTO tags (name) VALUES (?)';
+                $results = $db->prepare($sql);
+                $results->bindValue(1, $tag, PDO::PARAM_STR);
+                $results->execute();
+                $tags_id = $db->lastInsertId();
+
+                $sql = "INSERT INTO the_link (entry_id, id_tags) VALUES ($entry_id, $id_tags)";
+                $db->query($sql);
+
+            } elseif (isset($tag)) {
+
+                $sql = 'SELECT id FROM tags WHERE name = ?';
+                $results = $db->prepare($sql);
+                $results->bindValue(1, $tag, PDO::PARAM_STR);
+                $results->execute();
+                $id_tags = $results->fetch();
+                $id_tags = $id_tags[0];
+
+                $sql = "INSERT INTO the_link (entry_id, id_tags) VALUES ($entry_id, $id_tags)";
+                $db->query($sql);
+            }
+        }
+
+        $db->commit();
+
+    } catch (Exception $e) {
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        $db->rollBack();
         return false;
     }
-        echo "Unable to update DB" . $e->getMessage();
-        die(); 
-    
-
-function update_entry_by_id($id, $title, $date, $time_spent, $learned, $resources){
-    include ("connection.php");
-    
-    try {
-        $sql = "UPDATE entries SET title = :title, date = :date, time_spent = :time_spent, learned = :learned, resources = :resources WHERE id = :id";
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam(':id',$id,PDO::PARAM_INT);
-        $stmt->bindParam(':title',$title,PDO::PARAM_STR);
-        $stmt->bindParam(':date',$date,PDO::PARAM_STR);
-        $stmt->bindParam(':time_spent',$time_spent,PDO::PARAM_STR);
-        $stmt->bindParam(':learned',$learned,PDO::PARAM_STR);
-        $stmt->bindParam(':resources',$resources,PDO::PARAM_STR);
-        $stmt->execute();
-        if($stmt->rowCount() == 1){
-            return true;
-            
-        } else {
-            
-            return false;
-            
-        }
-    } catch (Exception $e) {
-        echo "Unable to query DB" . $e->getMessage();
-        die();
-    }
-}
-function get_entry_with_id($id){
-    include ("connection.php");
-    try {
-        $sql = "SELECT * FROM entries WHERE id = :id";
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam(':id',$id,PDO::PARAM_INT);
-        $stmt->execute();
-        $results = $stmt->fetch(PDO::FETCH_ASSOC);
-        
-        return $results;
-    } catch (Exception $e) {
-        echo "Unable to query DB" . $e->getMessage();
-        die();
-    }
-}
-function delete_entry($id){
-    include ("connection.php");
-    
-    try {
-        $sql = "DELETE FROM entries WHERE id = :id";
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam(':id',$id,PDO::PARAM_INT);
-        $stmt->execute();
-        
-        if($stmt->rowCount() == 1){
-            return true;
-            
-        } else {
-            
-            return false;
-            
-        }
-    } catch (Exception $e) {
-        echo "Unable to query DB" . $e->getMessage();
-        die();
-    }
+    return true;
 }
 
-    
-?>
+function get_entry($id)
+{
+    include 'connection.php';
+
+    $sql = "SELECT entries.id, entries.title, entries.date, entries.time_spent, entries.learned, entries.resources, GROUP_CONCAT(tags.name, ' ') AS name
+        FROM entries 
+        LEFT OUTER JOIN the_link ON entries.id = the_link.entry_id 
+        LEFT OUTER JOIN tags ON id_link_tags = id_tags
+        WHERE entries.id = ?
+        GROUP BY entries.id ORDER BY date DESC";
+
+    try {
+
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $id, PDO::PARAM_INT);
+        $results->execute();
+
+    } catch (Exception $e) {
+
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        return false;
+    }
+    return $results->fetch();
+}
+
+function edit_entry($id, $title, $date, $timeSpent, $learned, $resources = null, $tags = null)
+{
+    include 'connection.php';
+
+    try {
+
+        $db->beginTransaction();
+
+        $sql = 'DELETE FROM a_link WHERE entry_id = ?';
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $id, PDO::PARAM_INT);
+        $results->execute();
+
+        $sql = 'UPDATE entries SET title = ?, date = ?, time_spent = ?, learned = ?, resources = ? WHERE id = ?';
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $title, PDO::PARAM_STR);
+        $results->bindValue(2, $date, PDO::PARAM_STR);
+        $results->bindValue(3, $timeSpent, PDO::PARAM_STR);
+        $results->bindValue(4, $learned, PDO::PARAM_STR);
+        $results->bindValue(5, $resources, PDO::PARAM_STR);
+        $results->bindValue(6, $id, PDO::PARAM_INT);
+        $results->execute();
+
+        foreach ($tags as $tag) {
+
+            if (isset($tag) && (in_array($tag, get_tags())) == false) {
+                $sql = 'INSERT INTO tags (name) VALUES (?)';
+                $results = $db->prepare($sql);
+                $results->bindValue(1, $tag, PDO::PARAM_STR);
+                $results->execute();
+                $tags_id = $db->lastInsertId();
+
+                $sql = "INSERT INTO a_link (entry_id, id_tags) VALUES ($id, $id_tags)";
+                $db->query($sql);
+            } elseif (isset($tag)) {
+                $sql = 'SELECT id FROM tags WHERE name = ?';
+                $results = $db->prepare($sql);
+                $results->bindValue(1, $tag, PDO::PARAM_STR);
+                $results->execute();
+                $tags_id = $results->fetch();
+                $tags_id = $tags_id[0];
+
+                $sql = "INSERT INTO a_link (entry_id, id_tags) VALUES ($id, $id_tags)";
+                $db->query($sql);
+            }
+        }
+
+        $db->commit();
+
+    } catch (Exception $e) {
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        $db->rollBack();
+        return false;
+    }
+    return true;
+}
+
+function delete_entry($id) {
+
+    include 'connection.php';
+
+    try {
+        
+        $db->beginTransaction();
+
+        $sql = 'DELETE FROM entries WHERE id = ?';
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $id, PDO::PARAM_INT);
+        $results->execute();
+
+        $sql = 'DELETE FROM a_link WHERE entry_id = ?';
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $id, PDO::PARAM_INT);
+        $results->execute();
+
+        $db->commit();
+
+    } catch (Exception $e) {
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        $db->rollBack();
+        return false;
+    }
+    return true;
+}
+
+function get_part_tag($name) 
+{
+    include 'connection.php';
+
+    $sql = 'SELECT entries.id, entries.title, entries.date, tags.name
+        FROM entries 
+        LEFT OUTER JOIN the_link ON entries.id = the_link.entry_id 
+        LEFT OUTER JOIN tags ON id_link_tags = id.tags
+        WHERE tags.name = ?
+        ORDER BY date DESC';
+
+    try {
+
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $name, PDO::PARAM_STR);
+        $results->execute();
+
+    } catch (Exception $e) {
+
+        echo 'ERROR!: ' . $e->getMessage() . ' 😕 <br>';
+        return false;
+    }
+    return $results->fetchAll(PDO::FETCH_ASSOC);
+}
